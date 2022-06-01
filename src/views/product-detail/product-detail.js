efbe2ea881fda5d9dccda963bf893ae66803e37c
sojeongsojeong-park <psj90972@naver.com>
2022-06-01 20:10:50 +0900

    Feat:장바구니 localStorage 구현중

--- src/views/product-detail/product-detail.js
+++ src/views/product-detail/product-detail.js
@@ -1,21 +1,33 @@
 const addCartBtn = document.getElementById("addCart");
 
+
+
 //'장바구니 추가'버튼을 누르면 alert창이 뜨면서 localStorage에 담기게 됩니다.
 addCartBtn.addEventListener("click", () => {
-
-  alert('장바구니에 추가되었습니다.');
-
   //localStorage에 저장되는 정보입니다.
   //제품명을 key 값으로 가지며, 제품명과 가격을 value로 가집니다.
   //추후 수량기능도 추가하여 반영할 예정입니다.
   const productName = document.getElementById('productName').innerText;
   const price = document.getElementById('price').innerText;
+  const productImage = document.getElementById("chocolate1").src;
+  const data = {};
+  data.name = productName;
+  data.price = price;
+  data.img = productImage;
 
-  //데이터를 뿌릴때는 '/'을 기준으로 split하여 페이지에 나타낼 예정입니다.
-  localStorage.setItem(`${productName}`, `${productName}/ ${price}` )
+  //localStorage에 같은 이름의 KEY 값이 있다면 이미 추가된 물품임을 알려줌.
+  for(let i = 0; i <= localStorage.length; i++){
+    const matchingItem = JSON.parse(localStorage.getItem(i));
+    console.log(matchingItem);
+    if(localStorage.length<=0){
+      alert('장바구니에 추가되었습니다.');
+      localStorage.setItem( localStorage.length + 1, JSON.stringify(data) );
+      break;
+    } else if(matchingItem == `${productName}`){
+      alert('이미 추가된 물품 입니다.')
+    }
+  }
+  
 })
 
-//Q. 나중에 API로 데이터를 받아올 때에도 이렇게 id와 id.value로 localStorage에 저장을해도 무방할까요?
-//Q. 로컬스토리지를 처음 사용해봐서 이렇게 사용하는 것이 맞는지 잘 모르겠습니다....! 
-//   특히, setItem을 하는 부분에 제품명, 가격, 수량을 저장해두고 장바구니 페이지에서 다시 페이지에 데이터를 펼쳐야하는데, 
-//   split으로 나누어 뿌리는 방법이 적절한지, 아니면 데이터 베이스의 아이디값을 저장해두고 다시 받아오는 방식이 가능한지 궁금합니다!
+
---
+++ src/views/shopping-cart/shopping-cart.js
@@ -0,0 +1,68 @@
+const selectedProduct = document.getElementById("selectedProduct")
+const selectedPrice = document.getElementById("productPrice");
+const modifyQuantity = document.querySelectorAll(".productQuantity");
+const chocolateImg = document.getElementById("chocolateImg");
+
+//localStorage의 데이터를 받아오기
+for(let i = 1; i <= localStorage.length; i++){
+  const print = JSON.parse(localStorage.getItem(i));
+  //localStorage의 가격 입력
+  selectedPrice.innerText = print.price;
+  chocolateImg.src = print.img;
+}
+
+//버튼을 누르면 증가, 감소
+const plus = document.getElementById("plusProductQuantity");
+const minus = document.getElementById("minusProductQuantity");
+const totalPrice = document.getElementById("totalPrice");
+const payProductQuantity = document.getElementById("payProductQuantity");
+const payProductPrice = document.getElementById("payProductPrice");
+const payShippingPrice = document.getElementById("payShippingPrice");
+const payTotalPrice = document.getElementById("payTotalPrice");
+
+//default값으로 1을 부여 
+let orderedQuantity = 1;
+modifyQuantity.forEach(i => i.innerText = orderedQuantity);
+payProductQuantity.innerText = orderedQuantity;
+//default 값으로 가격 * 수량을 보여줌
+totalPrice.innerText = selectedPrice.innerText;
+payProductPrice.innerText = selectedPrice.innerText;
+payShippingPrice.innerText = 3000;
+payTotalPrice.innerText = Number(payProductPrice.innerText) + Number(payShippingPrice.innerText);
+
+// + 버튼을 클릭하면 수량 증가
+plus.addEventListener("click", plusQuantity)
+//- 버튼을 클릭하면 수량 감소
+minus.addEventListener("click", minusQuantity)
+
+function plusQuantity(){
+  let modifying = ++orderedQuantity;
+
+  //수량 입력
+  modifyQuantity.forEach(i => i.innerText = modifying);
+
+  //주문 수량과 가격을 곱하여 해당 상품의 총 금액을 보여줌
+  totalPrice.innerText = print.price * modifying
+
+  //결제 정보창에도 반영
+  payProductQuantity.innerText = modifying;
+  payProductPrice.innerText = totalPrice.innerText;
+  payTotalPrice.innerText = Number(payProductPrice.innerText) + Number(payShippingPrice.innerText)
+}
+
+function minusQuantity(){
+  let modifying = --orderedQuantity;
+
+  //수량 입력
+  modifyQuantity.forEach(i => i.innerText = modifying);
+
+  //주문 수량과 가격을 곱하여 해당 상품의 총 금액을 보여줌
+  totalPrice.innerText = print.price * modifying
+
+  //결제 정보창에도 반영
+  payProductQuantity.innerText = modifying;
+  payProductPrice.innerText = totalPrice.innerText;
+  payTotalPrice.innerText = Number(payProductPrice.innerText) + Number(payShippingPrice.innerText)
+}
+
+

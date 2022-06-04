2f5de60c96eacf29807af280ab172a09eb563f60
이수정 <aqqlee@gamil.com>
2022-06-04 22:22:01 +0900

    Feat: (관리)주문정보를 변경

--- src/routers/order-router.js
+++ src/routers/order-router.js
@@ -14,9 +14,9 @@ orderRouter.post("/", loginRequired, async (req, res, next) => {
 		}
 
 		const userId = req.currentUserId;
-		const { ShipAddress, request, summaryTitle, orderitem, totalPrice, status } = req.body;
+		const { shipAddress, request, summaryTitle, orderitem, totalPrice, status } = req.body;
 
-		const newOrder = await orderService.addOrder({ userId, ShipAddress, request, summaryTitle, orderitem, totalPrice, status });
+		const newOrder = await orderService.addOrder({ userId, shipAddress, request, summaryTitle, orderitem, totalPrice, status });
 
 		res.status(201).json(newOrder);
 	} catch (error) {
@@ -64,6 +64,39 @@ orderRouter.get("/:userId/:orderId", loginRequired, async function (req, res, ne
 		}
 
 		res.status(200).json(order);
+	} catch (error) {
+		next(error);
+	}
+});
+
+// (관리)주문정보를 변경(배송정보,주문일부취소만 가능)
+orderRouter.patch("/:orderId", loginRequired, async function (req, res, next) {
+	try {
+		const userRole = await req.currentUserRole;
+		if (userRole !== "admin") {
+			console.log("basic-user 등급 유저의 전체 주문정보변경 요청이 거부됨");
+			throw new Error("권한이 없습니다.");
+		}
+
+		const orderId = req.params.orderId;
+		const { shipAddress, request, summaryTitle, orderitem, totalPrice, status } = req.body;
+
+		const toUpdate = {
+			...(shipAddress && { shipAddress }),
+			...(request && { request }),
+			...(summaryTitle && { summaryTitle }),
+			...(orderitem && { orderitem }),
+			...(totalPrice && { totalPrice }),
+			...(status && { status }),
+		};
+
+    console.log(toUpdate);
+
+		// 주문정보를 변경함
+		const updateOrderInfo = await orderService.setOrder(orderId, toUpdate);
+
+		// 업데이트 이후의 주문정보를 프론트에 보내 줌
+		res.status(200).json(updateOrderInfo);
 	} catch (error) {
 		next(error);
 	}
--- src/services/order-service.js
+++ src/services/order-service.js
@@ -8,10 +8,10 @@ class OrderService {
 
 	//새주문
 	async addOrder(orderInfo) {
-		const { userId, ShipAddress, request, summaryTitle, orderitem, totalPrice, status } = orderInfo;
+		const { userId, shipAddress, request, summaryTitle, orderitem, totalPrice, status } = orderInfo;
 		const orderId = await this.orderModel.newOrderId();
 
-		const newOrderInfo = { orderId, userId, ShipAddress, request, summaryTitle, orderitem, totalPrice, status };
+		const newOrderInfo = { orderId, userId, shipAddress, request, summaryTitle, orderitem, totalPrice, status };
 		const createdNewOrder = await this.orderModel.create(newOrderInfo);
 
 		return createdNewOrder;
@@ -33,6 +33,28 @@ class OrderService {
 		const order = orders.find((e) => e.orderId === orderId);
 		return order;
 	}
+
+  
+  //주문정보를 변경(배송정보,주문변경 가능)
+  async setOrder(orderId, toUpdate) {
+    // 주문정보가 변경 가능한 상태인지 확인
+    let order = await this.orderModel.findByOrderId(orderId);
+
+    // db에서 찾지 못한 경우, 에러 메시지 반환
+    if (!order) {
+      throw new Error('주문 내역이 없습니다. 다시 한 번 확인해 주세요.');
+    }
+
+    //status가 결제완료 이면 주문정보 변경가능, 배송준비중/발송완료일때는 status만 변경가능
+
+    console.log(toUpdate);
+    order = await this.orderModel.update({
+      orderId,
+      update: toUpdate,
+    });
+
+    return order;
+  }
 }
 
 const orderService = new OrderService(orderModel);

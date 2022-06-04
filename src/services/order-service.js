ccf0059f38c6600a4e9cc44dd986316413c3e8da
이수정 <aqqlee@gamil.com>
2022-06-04 17:51:28 +0900

    Fix: 경로변경, 주문번호생성 함수분리

--- src/services/order-service.js
+++ src/services/order-service.js
@@ -9,16 +9,7 @@ class OrderService {
 	//새주문
 	async addOrder(orderInfo) {
 		const { userId, ShipAddress, request, summaryTitle, orderitem, totalPrice, status } = orderInfo;
-//orderId생성 날짜 + 일련번호(총문서갯수를 세서 부여하므로 문서삭제시 중복번호 발생함)
-		const date = new Date();
-		const sYear = date.getFullYear();
-		let sMonth = date.getMonth() + 1;
-		let sDate = date.getDate();
-		sMonth = sMonth > 9 ? sMonth : "0" + sMonth;
-		sDate = sDate > 9 ? sDate : "0" + sDate;
-
-		const orderLength = await this.orderModel.count();
-		const orderId = String(sYear).slice(2) + sMonth + sDate + String(orderLength).padStart(6, 0);
+		const orderId = await this.orderModel.newOrderId();
 
 		const newOrderInfo = { orderId, userId, ShipAddress, request, summaryTitle, orderitem, totalPrice, status };
 		const createdNewOrder = await this.orderModel.create(newOrderInfo);

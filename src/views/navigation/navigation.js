93289373d31a4771f563d6aaf7a221b3e589b82c
dbwls387 <dbwls387@gmail.com>
2022-06-05 01:26:30 +0900

    Resolves: innerHTML 삭제

--- src/views/navigation/navigation.js
+++ src/views/navigation/navigation.js
@@ -1,18 +1,49 @@
 const nav = document.querySelector(".nav");
-nav.innerHTML = `  
-      <div class="navbar-logo">
-        <a class="navbar-brand" href="/">
-          <i class="fas fa-cookie-bite"></i>
-          <span class="has-text-link">이상한 나라의 초콜릿</span>
-        </a>
-      </div>
-      <div class="navbar">
-        <ul class="navbar-items">
-          <li><a href="/login">로그인</a></li>
-          <li><a href="register">회원가입</a></li>
-          <li>
-            <a href="/cart"> <i class="fas fa-cart-shopping"></i> 카트 </a>
-          </li>
-        </ul>
-      </div>
-  `;
+
+// logo 부분
+const navbarLogo = document.createElement("div");
+navbarLogo.setAttribute("class", "navbarLogo");
+
+const navbarBrand = document.createElement("a");
+navbarBrand.setAttribute("class", "navbarBrand");
+navbarBrand.setAttribute("href", "/");
+navbarLogo.appendChild(navbarBrand);
+
+const fontImg = document.createElement("i");
+fontImg.setAttribute("class", "fas fa-cookie-bite");
+
+const textLink = document.createElement("span");
+textLink.setAttribute("class", "textLink");
+const textNode = document.createTextNode(" 이상한 나라의 초콜릿");
+textLink.appendChild(textNode);
+
+navbarBrand.appendChild(fontImg);
+navbarBrand.appendChild(textLink);
+
+// item 부분
+const navbar = document.createElement("div");
+navbar.setAttribute("class", "navbar");
+
+const navbarItems = document.createElement("ul");
+navbarItems.setAttribute("class", "navbarItems");
+
+const liLoginTag = document.createElement("li");
+const aLoginTag = document.createElement("a");
+const textLogin = document.createTextNode("로그인");
+liLoginTag.appendChild(aLoginTag);
+aLoginTag.appendChild(textLogin);
+aLoginTag.setAttribute("href", "/login");
+
+const liCartTag = document.createElement("li");
+const aCartTag = document.createElement("a");
+const textCart = document.createTextNode("장바구니");
+liCartTag.appendChild(aCartTag);
+aCartTag.appendChild(textCart);
+aCartTag.setAttribute("href", "/cart");
+
+navbarItems.appendChild(liLoginTag);
+navbarItems.appendChild(liCartTag);
+
+// .nav에 넣기
+nav.appendChild(navbarLogo);
+nav.appendChild(navbarItems);

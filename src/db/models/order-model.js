1bc99eb6d41a3ce3fc722ac724912bd23c2d080b
이수정 <aqqlee@gamil.com>
2022-06-04 17:50:01 +0900

    Feat: 주문번호 생성 함수 작성

--- src/db/models/order-model.js
+++ src/db/models/order-model.js
@@ -38,6 +38,23 @@ export class OrderModel {
     const counts = await Order.count()
     return counts;
   }
+  
+  //주문번호생성
+  async newOrderId(){
+    //orderId생성 날짜 + 일련번호(총문서갯수를 세서 부여하므로 문서삭제시 중복번호 발생함)
+    const date = new Date();
+    const sYear = date.getFullYear();
+    let sMonth = date.getMonth() + 1;
+    let sDate = date.getDate();
+  
+    sMonth = sMonth > 9 ? sMonth : "0" + sMonth;
+    sDate = sDate > 9 ? sDate : "0" + sDate;
+  
+    const orderLength = await Order.count();
+    const orderId = String(sYear).slice(2) + sMonth + sDate + String(orderLength).padStart(6, 0);
+  
+    return orderId;
+  }
 }
 
 const orderModel = new OrderModel();

a9cef3f9a1d930e54263dd1752d4369670a5d311
sojeongsojeong-park <psj90972@naver.com>
2022-06-05 17:09:07 +0900

    Feat: 장바구니 구현 및 상품 추가 페이지 장바구니 페이지의 기능을 완료하였고, 상품추가 페이지에서 서버로 데이터를 전송하는 기능을 제외하고 구현을 마쳤습니다.

--- src/views/product-detail/product-detail.js
+++ src/views/product-detail/product-detail.js
@@ -1,35 +1,57 @@
 const addCartBtn = document.getElementById("addCart");
+const productName = document.getElementById('productName').innerText;
+const price = document.getElementById('price').innerText;
+const productImage = document.getElementById("chocolate1").src;
 
+const CARTLIST_KEY = "cartList";
+//임시 장바구니
+let addCartList = [];
+//최종 장바구니
+let uniqCartList = [];
+function saveCartList(){
+  localStorage.setItem(CARTLIST_KEY, JSON.stringify(uniqCartList));
+}
 //'장바구니 추가'버튼을 누르면 alert창이 뜨면서 localStorage에 담기게 됩니다.
-addCartBtn.addEventListener("click", () => {
-
-
-  // alert('장바구니에 추가되었습니다.');
+addCartBtn.addEventListener("click", (e) => {
+  e.preventDefault();
 
   //localStorage에 저장되는 정보입니다.
   //제품명을 key 값으로 가지며, 제품명과 가격을 value로 가집니다.
   //추후 수량기능도 추가하여 반영할 예정입니다.
-  const productName = document.getElementById('productName').innerText;
-  const price = document.getElementById('price').innerText;
-  const productImage = document.getElementById("chocolate1").src;
+
   const data = {};
   data.name = productName;
   data.price = price;
   data.img = productImage;
 
-  //localStorage에 같은 이름의 KEY 값이 있다면 이미 추가된 물품임을 알려줌.
+  addCartList.push(data);
 
-//localStorage.setItem( 2, JSON.stringify({name: "loyal chocolate", price: 4000, img:"http://127.0.0.1:5500/src/views/elice-rabbit.png"})) 
+  const isadded = uniqCartList.find(e => e.name === productName);
+  if(isadded){
+    alert("이미 추가된 상품입니다!")
+  } else{
+    alert("장바구니에 추가되었습니다.");
+  }
 
-  const keys = Object.keys(window.localStorage); //이건그냥 안쓰고 포문으로 해도될것같은데 이왕 검색했으니까 넣음
-  console.log(keys);
-  const values = keys.map(e => JSON.parse(localStorage.getItem(localStorage.key(parseInt(e)-1))))
-  console.log(values);
 
-  const isItem  = values.map( e => e.name == `${productName}` ?  "있음" : "없음")
-  console.log(isItem);
-  if(isItem.includes("있음")){
-    alert('이미 추가된 물품 입니다.')    
-    }else{localStorage.setItem( localStorage.length + 1, JSON.stringify(data) );
-      alert('장바구니에 추가되었습니다.');}
+  makeAddCartList();
+  makeUniq();
+  saveCartList();
 })
+
+function makeAddCartList(){
+  let existedData = JSON.parse(localStorage.getItem(CARTLIST_KEY));
+  if(existedData !== null){
+    existedData.forEach(e => {
+      addCartList.push(e)
+    });
+  }
+}
+
+function makeUniq(){
+  const map = new Map();
+  for(const item of addCartList){
+    map.set(JSON.stringify(item), item);
+  }
+  uniqCartList = [...map.values()];
+};

fd7f21d6cbb8ae1322600106bca1f042f8a194fb
dbwls387 <dbwls387@gmail.com>
2022-06-06 00:59:16 +0900

    Refactor: form tag 수정 및 userId 저장

--- src/views/my-information/my-information.js
+++ src/views/my-information/my-information.js
@@ -1,1 +1,17 @@
 import * as Api from "/api.js";
+
+const btn = document.querySelector(".submitEdit");
+
+btn.addEventListener("click", handleSubmit);
+
+async function handleSubmit(e) {
+  e.preventDefault();
+  try {
+    const getUserId = sessionStorage.getItem("userId");
+    const userr = await Api.get(`/api/users/${getUserId}`);
+    console.log(userr);
+  } catch (err) {
+    console.error(err.stack);
+    alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`);
+  }
+}

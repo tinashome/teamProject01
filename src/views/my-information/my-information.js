5bb9e8ab211409218400f56f7dfa27dbb9ab9e68
dbwls387 <dbwls387@gmail.com>
2022-06-07 04:06:46 +0900

    Resolves: #59 (모달창 구현)

--- src/views/my-information/my-information.js
+++ src/views/my-information/my-information.js
@@ -9,6 +9,10 @@ const postalCodeInput = document.querySelector("#postalCodeInput");
 const addressInput = document.querySelector("#addressInput");
 const detailAddressInput = document.querySelector("#detailAddressInput");
 const submitEdit = document.querySelector("#submitEdit");
+const delMyInfoBtn = document.querySelector("#delMyInfoBtn");
+const searchBtn = document.querySelector(".searchBtn");
+
+const getUserId = sessionStorage.getItem("userId");
 
 // 주소 찾기 api
 async function searchAddress(e) {
@@ -24,33 +28,40 @@ async function searchAddress(e) {
 }
 
 // 기존 정보 불러오기
-const getUserId = sessionStorage.getItem("userId");
 async function printInformation() {
   try {
     const userInfo = await Api.get(`/api/users/${getUserId}`);
 
     fullNameInput.value = userInfo.fullName;
-    phoneNumberInput.value = userInfo.phoneNumber;
-    postalCodeInput.value = userInfo.address.postalCode;
-    addressInput.value = userInfo.address.address1;
-    detailAddressInput.value = userInfo.address.address2;
-
-    console.log(userInfo);
+    phoneNumberInput.value = userInfo.phoneNumber ? userInfo.phoneNumber : null;
+    postalCodeInput.value = userInfo.address
+      ? userInfo.address.postalCode
+      : null;
+    addressInput.value = userInfo.address ? userInfo.address.address1 : null;
+    detailAddressInput.value = userInfo.address
+      ? userInfo.address.address2
+      : null;
+    return userInfo.password;
   } catch (err) {
     console.error(err.stack);
     alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`);
   }
 }
 
-// 정보 수정하기
-const searchBtn = document.querySelector(".searchBtn");
-searchBtn.addEventListener("click", searchAddress);
+// 비밀번호 입력 모달창
+async function close(e) {
+  e.preventDefault();
+  document.querySelector(".modal").classList.add("hidden");
+}
+document.querySelector(".closeBtn").addEventListener("click", close);
+document.querySelector(".closeBtn").addEventListener("click", handleSubmit);
 
+// 정보 수정하기
 async function handleSubmit(e) {
   e.preventDefault();
 
-  const phoneNumber = phoneNumberInput.value;
   const currentPassword = passwordInput.value;
+  const phoneNumber = phoneNumberInput.value;
   const password = newPasswordInput.value;
   const passwordConfirm = passwordConfirmInput.value;
   const postalCode = postalCodeInput.value;
@@ -64,7 +75,6 @@ async function handleSubmit(e) {
   if (!isPasswordValid) {
     return alert("비밀번호는 4글자 이상이어야 합니다.");
   }
-
   if (!isPasswordSame) {
     return alert("비밀번호가 일치하지 않습니다.");
   }
@@ -81,7 +91,7 @@ async function handleSubmit(e) {
       },
     };
 
-    const result = await Api.patch("/api/users/", getUserId, data);
+    await Api.patch("/api/users/", getUserId, data);
 
     alert("정보가 수정되었습니다. ");
   } catch (err) {
@@ -90,5 +100,25 @@ async function handleSubmit(e) {
   }
 }
 
+// 탈퇴하기 (회원 정보 삭제)
+async function deleteMyInfo() {
+  try {
+    const currentPassword = passwordInput.value;
+    const submitPassword = { currentPassword };
+
+    await Api.delete("/api/users/", getUserId, submitPassword);
+    location.replace = "/";
+  } catch (err) {
+    console.error(err.stack);
+    alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`);
+  }
+}
+
 printInformation();
-submitEdit.addEventListener("click", handleSubmit);
+submitEdit.addEventListener("click", async function modalOpen(e) {
+  e.preventDefault();
+  passwordInput.focus();
+  document.querySelector(".modal").classList.remove("hidden");
+});
+delMyInfoBtn.addEventListener("click", deleteMyInfo);
+searchBtn.addEventListener("click", searchAddress);

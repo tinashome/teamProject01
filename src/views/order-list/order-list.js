8d7cbdd8aa5394700840bded36382a2fa43286ec
unknown <chn0202@gmail.com>
2022-06-07 17:40:34 +0900

    Feat: 오더리스트 이벤트 처리중

--- src/views/order-list/order-list.js
+++ src/views/order-list/order-list.js
@@ -1,104 +1,123 @@
+import * as Api from "/api.js";
 // 테스트용
-const date = [
-  {
-    date: "2020",
-    listProduct: "dd",
-    userStatus: "준비중",
-    productId: 1,
-  },
-  {
-    date: "2020",
-    listProduct: "1",
-    userStatus: "준비중",
-    productId: 2,
-  },
-  {
-    date: "2020",
-    listProduct: "2",
-    userStatus: "준비중",
-    productId: 3,
-  },
-  {
-    date: "2020",
-    listProduct: "3",
-    userStatus: "준비중",
-    productId: 4,
-  },
-  {
-    date: "2020",
-    listProduct: "4",
-    userStatus: "준비중",
-    productId: 7,
-  },
-  {
-    date: "2020",
-    listProduct: "5",
-    userStatus: "준비중",
-    productId: 5,
-  },
-  {
-    date: "2020",
-    listProduct: "6",
-    userStatus: "준비중",
-    productId: 6,
-  },
-];
+
+async function getOrder() {
+  try {
+    const token = sessionStorage.getItem("userId");
+
+    const a = await Api.get(`/api/orders/${token}`);
+
+    datefnc(a);
+  } catch (err) {
+    console.error(err.stack);
+    alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`);
+  }
+}
+getOrder();
 
 // 날짜, 상품리스트, 준비상태 불러오기
+const data = [];
+function datefnc(orderDate) {
+  for (let i = 0; i < orderDate.length; i++) {
+    let dateInfo = orderDate[i].createdAt;
+    const listProduct = orderDate[i].summaryTitle;
+    const productId = orderDate[i].orderId;
+    const userStatusData = "준비중";
+    const date = dateInfo.substr(0, 10);
 
-//  **********get으로 서버 데이터 받아오기**********
-date.forEach((element) => {
-  // 임시 데이터 받아오기
-  const date = element.date;
-  const listProduct = element.listProduct;
-  const userStatusData = element.userStatus;
-  const productId = element.productId;
+    data.push({
+      date,
+      userStatusData,
+      listProduct,
+      productId,
+    });
+  }
+  data.forEach((element) => {
+    const date = element.date;
+    const listProduct = element.listProduct;
+    const userStatusData = element.userStatusData;
+    const productId = element.productId;
 
-  // 추가할 요소 생성
+    // 추가할 요소 생성
 
-  //전체를 감싸줄 컨테이너
-  const wrapper = document.createElement("div");
-  wrapper.classList.add("creatOrderInquiryWrapper");
-  // 백에서 받아온 id
-  // wrapper.setAttribute("id", "ssss")
+    // 전체를 감싸줄 컨테이너
 
-  // 주문날짜
-  const orderDate = document.createElement("div");
-  orderDate.classList.add("orderInfo");
-  orderDate.textContent = date;
+    const wrapper = document.createElement("div");
+    wrapper.classList.add("creatOrderInquiryWrapper");
 
-  // 상품정보
-  const orderProduct = document.createElement("div");
-  orderProduct.classList.add("orderInfo");
-  orderProduct.textContent = listProduct;
+    const orderDate = document.createElement("div");
+    orderDate.classList.add("orderInfo");
+    orderDate.textContent = date;
 
-  // 상태정보
-  const userStatus = document.createElement("div");
-  userStatus.classList.add("orderInfo");
-  userStatus.textContent = userStatusData;
+    const orderProduct = document.createElement("div");
+    orderProduct.classList.add("orderInfo");
+    orderProduct.textContent = listProduct;
 
-  // 버튼 컨테이너
-  const btnWrapper = document.createElement("div");
-  btnWrapper.classList.add("orderInfo");
+    const userStatus = document.createElement("div");
+    userStatus.classList.add("orderInfo");
+    userStatus.textContent = userStatusData;
 
-  // 버튼
-  // id값 가져와서 버튼에 id값 넣기
-  const btn = document.createElement("button");
-  btn.setAttribute("id", `${productId}`);
-  btn.setAttribute("class", "cencleOrder");
-  btn.textContent = "주문 취소";
+    const btnWrapper = document.createElement("div");
+    btnWrapper.classList.add("orderInfo");
 
-  //화면에 보이게 할려고
-  btnWrapper.append(btn);
-  wrapper.append(orderDate, orderProduct, userStatus, btnWrapper);
+    const btn = document.createElement("button");
+    btn.setAttribute("id", `${productId}`);
+    btn.setAttribute("class", "cencleOrder");
+    btn.textContent = "주문 취소";
 
-  const ordersContainer = document.getElementById("section");
-  ordersContainer.append(wrapper);
-});
+    btnWrapper.append(btn);
+    wrapper.append(orderDate, orderProduct, userStatus, btnWrapper);
+
+    const ordersContainer = document.getElementById("section");
+    ordersContainer.append(wrapper);
+  });
+}
+
+// 추가할 요소 생성
+
+// 전체를 감싸줄 컨테이너
+// const wrapper = document.createElement("div");
+// wrapper.classList.add("creatOrderInquiryWrapper");
+// 백에서 받아온 id
+// wrapper.setAttribute("id", "ssss")
+
+// 주문날짜
+// const orderDate = document.createElement("div");
+// orderDate.classList.add("orderInfo");
+// orderDate.textContent = date;
+
+// 상품정보
+// const orderProduct = document.createElement("div");
+// orderProduct.classList.add("orderInfo");
+// orderProduct.textContent = listProduct;
+
+// 상태정보
+// const userStatus = document.createElement("div");
+// userStatus.classList.add("orderInfo");
+// userStatus.textContent = userStatusData;
+
+// 버튼 컨테이너
+// const btnWrapper = document.createElement("div");
+// btnWrapper.classList.add("orderInfo");
+
+// 버튼
+// id값 가져와서 버튼에 id값 넣기
+// const btn = document.createElement("button");
+// btn.setAttribute("id", `${productId}`);
+// btn.setAttribute("class", "cencleOrder");
+// btn.textContent = "주문 취소";
+
+//화면에 보이게 할려고
+// btnWrapper.append(btn);
+// wrapper.append(orderDate, orderProduct, userStatus, btnWrapper);
+
+// const ordersContainer = document.getElementById("section");
+// ordersContainer.append(wrapper);
+// });
 
 // 여러개의 클릭 버튼 구현
 const button = document.querySelectorAll(".cencleOrder");
-
+console.log(button);
 // 서버에서 id값 받아오면 사용할 id
 const cancleBtn = [];
 button.forEach((btn) => {

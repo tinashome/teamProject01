91e8baa86074174ce6092211b6afc0c532d0f05f
elesis-sieghart <elesis@github.com>
2022-06-07 22:43:46 +0900

    Fix: add product 성공

--- src/views/add-product/add-product.js
+++ src/views/add-product/add-product.js
@@ -2,32 +2,46 @@ import * as Api from '../../../api.js'
 
 
 const form = document.querySelector("form");
+const fileInput = document.querySelector('.inputFile') ;
+
+
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  alert("제품 정보가 저장되었습니다.");
+e.preventDefault();
+alert("제품 정보가 저장되었습니다.");
 
-  //데이터 만들기
-  let data = new FormData(form.elements);
-  // for(const item of form.elements){
-  //   switch(item.type){
-  //     case 'text':
-  //       data[item.name] = item.value;
-  //       break;
-  //     case 'file':
-  //       data[item.name] = item.value;
-  //       break;
-  //   }
-  //   switch(item.localName){
-  //     case 'select':
-  //       data[item.name] = item.value;
-  //       break;
-  //     case 'textarea':
-  //       data[item.name] = item.value;
-  //       break;
-  //   }
-  // }
-  console.log(data);
-  Api.postImg("/api/product",data);
+const formData = new FormData();
 
+//데이터 만들기
+
+for (const item of form.elements) {
+  switch (item.type) {
+    case "number":
+      formData.append(item.name, item.value);
+      break;
+    case "text":
+      formData.append(item.name, item.value);
+      break;
+    case "file":
+      formData.append('upload', fileInput.files[0]);
+      break;
+  }
+  switch (item.localName) {
+    case "select":
+    formData.append(item.name, item.value);
+    break;
+    case "textarea":
+    formData.append(item.name, item.value);
+    break;
+  }
+}
+
+for (let key of formData.keys()) {
+  console.log(key);
+}
+for (let value of formData.values()) {
+  console.log(value);
+}
+
+// console.log(formData);
+Api.postImg("/api/product", formData);
 });
-

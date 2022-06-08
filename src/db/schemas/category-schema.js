fa44d4703c93e6cf4028887540968d2c6e21f6df
elesis-sieghart <elesis@github.com>
2022-06-08 21:28:35 +0900

    Fix: categories to category

--- src/db/schemas/category-schema.js
+++ src/db/schemas/category-schema.js
@@ -6,16 +6,16 @@ const CategorySchema = new Schema(
       type: String, // 이름: 대분류, 중분류, 소분류
       unique: true,
     },
-    level: {
-      type: Number, // 분류: 0, 1 ,2
-    },
+    // level: {
+    //   type: Number, // 분류: 0, 1 ,2
+    // },
     info: {
       type: String, // 설명: 대분류 입니다.
     },
-    belongTo: {
-      type: Schema.Types.ObjectId,
-      ref: 'categories',
-    },
+    // belongTo: {
+    //   type: Schema.Types.ObjectId,
+    //   ref: 'categories',
+    // },
   },
   {
     collection: "categories",

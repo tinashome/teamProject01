fb6023612aa977fb3800c37757cad577cfc80767
[SW2/부산]박소정 <psoj90972@gmail.com>
2022-06-09 06:42:41 +0000

    Feat: 카테고리 추가와 상품 추가 기능을 완료하였습니다.

--- src/views/add-product/add-product.js
+++ src/views/add-product/add-product.js
@@ -1,5 +1,26 @@
 import * as Api from "../api.js";
 
+const selectCategory = document.querySelector("#category");
+
+async function getData() {
+  try {
+    const data = await Api.get("/api/categories");
+    console.log(data);
+
+    for (let i = 0; i < data.length; i++) {
+      console.log(data[i].name, data[i]._id);
+      let category = document.createElement("option");
+      category.textContent = data[i].name;
+      category.value = data[i]._id;
+      selectCategory.after(category);
+    }
+  } catch (err) {
+    console.error(err.stack);
+  }
+}
+
+getData();
+
 const fileInput = document.querySelector("#inputFile");
 const fileName = document.querySelector("#fileName");
 const form = document.querySelector("form");

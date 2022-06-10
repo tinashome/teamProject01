83f9f7555644361969372bce5c70311704e75483
sojeongsojeong-park <psj90972@naver.com>
2022-06-10 22:58:05 +0900

    Feat: 오류 수정

--- src/views/shopping-cart/shopping-cart.js
+++ src/views/shopping-cart/shopping-cart.js
@@ -10,9 +10,7 @@ const totalQuantityArrForPay = [];
 //localStorage의 데이터를 받아오기
 //printArr에 배열로 데이터들을 저장하여 반복문을 돌면서 데이터 펴기
 const printArr = [];
-const localStorageItem = JSON.parse(
-  localStorage.getItem("cartLitargetNumber = st")
-);
+const localStorageItem = JSON.parse(localStorage.getItem("cartList"));
 for (let i = 0; i < localStorageItem.length; i++) {
   printArr.push(localStorageItem[i]);
 }

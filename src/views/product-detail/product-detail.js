79173601595287d8f68cc9c0c4c5b740fd5e120b
이수정 <aqqlee@gamil.com>
2022-06-17 17:51:08 +0900

    Docs: add to gitignore

--- src/views/product-detail/product-detail.js
+++ src/views/product-detail/product-detail.js
@@ -2,6 +2,7 @@ import * as Api from "/api.js";
 import { addCommas } from "/useful-functions.js";
 
 const addCartBtn = document.querySelector("#addCart");
+const buyDirectBtn = document.querySelector("#buyDirect");
 const productName = document.querySelector("#productName");
 const price = document.querySelector("#price");
 const productImage = document.querySelector("#chocolate");
@@ -11,135 +12,96 @@ const decreaseBtn = document.querySelector("#decreaseBtn");
 const increaseBtn = document.querySelector("#increaseBtn");
 const numInput = document.querySelector("#numInput");
 
+const cartDivCloseBtn = document.querySelector("#cartDivCloseBtn");
+const cartDiv = document.querySelector("#cartDiv");
+const modal = document.querySelector("#modal");
+const goToCart = document.querySelector("#goToCart");
+const closeCartDiv = document.querySelector("#closeCartDiv");
+
 const path = window.location.pathname.split("/");
 const productId = path[path.length - 2];
 
-async function getData() {
-  try {
-    const data = await Api.get(`/api/product/${productId}`);
-    productName.textContent = data.name;
-    price.textContent = addCommas(data.price);
-    productImage.src = data.img;
-    productDescription.textContent = data.detail;
-    productSummary.textContent = data.summary;
-    numQuantity.textContent = data.quantity;
-  } catch (err) {
-    console.error(err.stack);
-  }
-}
+cartDivCloseBtn.addEventListener("click", cartDivToggle);
+closeCartDiv.addEventListener("click", cartDivToggle);
+goToCart.addEventListener("click", () => {
+	window.location.href = "/buydirectorder";
+});
 
-getData();
-
+//제품정보 저장
 const reqProductInfo = await fetch(`/api/product/${productId}`, {
 	headers: { Authorization: `Bearer ${sessionStorage.getItem("token")}` },
 });
-const prodictInfo = await reqProductInfo.json()
+const productInfo = await reqProductInfo.json();
+
+getData();
+removeBuyDirect();
+buyDirectBtn.addEventListener("click", buyDirect);
+addCartBtn.addEventListener("click", () => {
+	addCart(productInfo._id, productInfo.price, numInput.value);
+});
+
+async function getData() {
+	try {
+		const data = await Api.get(`/api/product/${productId}`);
+		productName.textContent = data.name;
+		price.textContent = addCommas(data.price);
+		productImage.src = data.img;
+		productDescription.textContent = data.detail;
+		productSummary.textContent = data.summary;
+		numQuantity.textContent = data.quantity;
+	} catch (err) {
+		console.error(err.stack);
+	}
+}
+
+//바로구매하기
+async function buyDirect() {
+	await localStorage.setItem("buyDirect", JSON.stringify({ id: productInfo._id, price: productInfo.price, quantity: numInput.value }));
+	window.location.href = "/buydirectorder";
+}
+//로컬저장소의 바로구매 비우기
+async function removeBuyDirect() {
+	await localStorage.removeItem("buyDirect");
+}
 
 decreaseBtn.addEventListener("click", () => {
-	if (Number(numInput.value) !== 1){
-    numInput.value = Number(numInput.value) - 1;
-    price.textContent = addCommas(prodictInfo.price * numInput.value)
-  }
+	const curNumInput = Number(numInput.value);
+	if (curNumInput !== 1) {
+		numInput.value = curNumInput - 1;
+		price.textContent = addCommas(productInfo.price * numInput.value);
+	}
 });
 increaseBtn.addEventListener("click", () => {
-  if (Number(numInput.value) !== Number(numQuantity.textContent)){
-    numInput.value = Number(numInput.value) + 1;
-    price.textContent = addCommas(prodictInfo.price * numInput.value)
-  }
+	const curNumInput = Number(numInput.value);
+	if (curNumInput !== Number(numQuantity.textContent)) {
+		numInput.value = curNumInput + 1;
+		price.textContent = addCommas(productInfo.price * numInput.value);
+	}
 });
 
-
-const CARTLIST_KEY = "cartList";
-// //임시 장바구니
-let addCartList = [];
-// //최종 장바구니
-let uniqCartList = [];
-function saveCartList() {
-  uniqCartList.sort(function (a, b) {
-    if (a.id > b.id) {
-      return 1;
-    }
-    if (a.id < b.id) {
-      return -1;
-    }
-    return 0;
-  });
-  if (uniqCartList.includes(null)) {
-    const finalCart = uniqCartList.filter((e) => e !== null);
-    localStorage.setItem(CARTLIST_KEY, JSON.stringify(finalCart));
-  } else {
-    localStorage.setItem(CARTLIST_KEY, JSON.stringify(uniqCartList));
-  }
-}
-//'장바구니 추가'버튼을 누르면 alert창이 뜨면서 localStorage에 담기게 됩니다.
-addCartBtn.addEventListener("click", async (e) => {
-  e.preventDefault();
-  const data = await Api.get(`/api/product/${productId}`);
-
-  // localStorage에 저장되는 정보입니다.
-  // 제품명을 key 값으로 가지며, 제품명과 가격을 value로 가집니다.
-  // 추후 수량기능도 추가하여 반영할 예정입니다.
-
-  const thisData = {};
-  thisData.name = data.name;
-  thisData.price = data.price;
-  thisData.img = data.img;
-  thisData.id = data._id;
-  thisData.quantity = 1;
-
-  makeAddCartList();
-  makeUniq();
-  addCartList.push(thisData);
-  addCartList = addCartList.filter((e) => e != null);
-  console.log(addCartList);
-
-  const isadded = uniqCartList.find((e) => e.id === data._id);
-  if (isadded) {
-    alert("이미 추가된 상품입니다!");
-  } else {
-    alert("장바구니에 추가되었습니다.");
-  }
-
-  makeAddCartList();
-  makeUniq();
-  saveCartList();
-});
-
-function makeAddCartList() {
-  let existedData = JSON.parse(localStorage.getItem(CARTLIST_KEY));
-  if (existedData !== null) {
-    existedData.forEach((e) => {
-      addCartList.push(e);
-    });
-  }
+async function addCart(id, price, quantity) {
+	cartDivToggle();
+	removeBuyDirect();
+	const myStorage = await window.localStorage;
+	const cartLen = myStorage.length;
+	for (let i = 0; i < cartLen; i++) {
+		const getStorage = JSON.parse(myStorage.getItem(i));
+		const getStorageQuantity = getStorage.quantity;
+		const setStorageQuantity = Number(getStorageQuantity) + Number(quantity);
+		if (id === getStorage.id) {
+			localStorage.setItem(i, JSON.stringify({ id, price, quantity: setStorageQuantity }));
+			return;
+		}
+	}
+	localStorage.setItem(cartLen, JSON.stringify({ id, price, quantity }));
 }
 
-function makeUniq() {
-  const map = new Map();
-  for (const item of addCartList) {
-    map.set(JSON.stringify(item), item);
-  }
-  uniqCartList = [...map.values()];
-}
-
-const buyDirect = document.querySelector("#buyDirect");
-buyDirect.addEventListener("click", async () => {
-  const token = sessionStorage.getItem("token");
-  if (!token) {
-    alert("상품 구매는 로그인한 유저만 가능합니다.");
-    window.location.href = "/login";
-  }
-
-  const data = await Api.get(`/api/product/${productId}`);
-
-  const thisData = {};
-  thisData.name = data.name;
-  thisData.price = data.price;
-  thisData.img = data.img;
-  thisData.id = data._id;
-  thisData.quantity = 1;
-
-  localStorage.setItem("buyDirect", JSON.stringify(thisData));
-
-  window.location.href = "/buydirectorder";
-});
+async function cartDivToggle() {
+	if (cartDiv.style.display === "none") {
+		cartDiv.style.display = "block";
+		modal.style.display = "block";
+	} else {
+		cartDiv.style.display = "none";
+		modal.style.display = "none";
+	}
+};
